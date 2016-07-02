43f9b08847bcbfb34fa0cf7655a3b8104bbe7606
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2016-07-02 22:02:45 +0900

    Initial Commit

---
+++ RxSwiftSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftSample
+//
+//  Created by 内村祐之 on 2016/07/02.
+//  Copyright © 2016年 ucuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
